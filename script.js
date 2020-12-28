7d1e0005fe711e8438d16a981ac09982aac1f8be
yousojeong <hielosan@naver.com>
2020-12-29 02:14:11 +0900

    +) 메뉴 수정

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
+// function topMenu(){
+//     document.getElementById("menuIcon").style.display = "none";
+//     document.getElementById("topMenu2").style.display = "block";
+// }
+// function post_option_display(){
+// 	$('#menuIcon').toggle();
+	
+// 	if($('#menuIcon').css("display", "block")){ 
+//         document.getElementById("menuIcon").style.display = "none";
+//         document.getElementById("menuIcon2").style.display = "block";
+//     }
+// }
+// function topMenu(){
+//     document.getElementById("menuIcon").style.display = "none";
+//     document.getElementById("menuIcon2").style.display = "block";
+// }
+
 function topMenu(){
-    document.getElementById("menuIcon").style.display = "none";
-    // document.getElementById("menuList").style.display = "block";
+	$('#A').toggle();
+	if($('#topMenu2').css("display", "block")){
+        $('#topMenu').css("display", "none")
+    }
+	if($('#topMenu').css("display", "block")){
+        $('#topMenu2').css("display", "none")
+    }
 }
